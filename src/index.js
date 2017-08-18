c7aaca5b6d255c7c7037329a862446cf0cfa5774
kateloschinina <ekaterina.loshchinina@gmail.com>
2017-08-18 17:49:24 +0100

    Src/index.js only needed to test footer locally.  🐿 v2.5.16

--- src/index.js
+++ src/index.js
@@ -4,42 +4,53 @@ import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import KmtHeaderContainer from './containers/kmt-header';
 
-class KmtHeader {
-  constructor (options = {}) {
-    if (!options.rootEl) {
-      options.rootEl = document.body;
-    } else if (typeof options.rootEl === 'string') {
-      options.rootEl = document.querySelector(options.rootEl);
-    }
+const rootEl = document.getElementById('root');
 
-    if (options.rootEl.hasAttribute('data-kmt-header--js')) {
-      return;
-    }
+const store = configureStore();
 
-    this.rootEl = options.rootEl;
-    this.rootEl.removeAttribute('data-kmt-header--no-js');
-    this.rootEl.setAttribute('data-kmt-header--js', '');
-
-    const store = configureStore(options.data);
-
-    ReactDOM.render(
-      <Provider store={store}>
-        <KmtHeaderContainer />
-      </Provider>,
-      this.rootEl);
-  }
-
-  static init (options = {}) {
-    if (!options.rootEl) {
-      options.rootEl = document.body;
-    } else if (typeof options.rootEl === 'string') {
-      options.rootEl = document.querySelector(options.rootEl);
-    }
-
-    if (!options.rootEl.hasAttribute('data-kmt-header--js')) {
-      return new KmtHeader(options);
-    }
-  }
-}
-
-export default KmtHeader;
+ReactDOM.render(
+	<Provider store={store}>
+		<KmtHeaderContainer />
+	</Provider>,
+	rootEl
+);
+//
+// class KmtHeader {
+//   constructor (options = {}) {
+//     if (!options.rootEl) {
+//       options.rootEl = document.body;
+//     } else if (typeof options.rootEl === 'string') {
+//       options.rootEl = document.querySelector(options.rootEl);
+//     }
+//
+//     if (options.rootEl.hasAttribute('data-kmt-header--js')) {
+//       return;
+//     }
+//
+//     this.rootEl = options.rootEl;
+//     this.rootEl.removeAttribute('data-kmt-header--no-js');
+//     this.rootEl.setAttribute('data-kmt-header--js', '');
+//
+//     const store = configureStore(options.data);
+//
+//     ReactDOM.render(
+//       <Provider store={store}>
+//         <KmtHeaderContainer />
+//       </Provider>,
+//       this.rootEl);
+//   }
+//
+//   static init (options = {}) {
+//     if (!options.rootEl) {
+//       options.rootEl = document.body;
+//     } else if (typeof options.rootEl === 'string') {
+//       options.rootEl = document.querySelector(options.rootEl);
+//     }
+//
+//     if (!options.rootEl.hasAttribute('data-kmt-header--js')) {
+//       return new KmtHeader(options);
+//     }
+//   }
+// }
+//
+// export default KmtHeader;
