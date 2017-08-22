13984c9135b6a4d0563bfc3ba8b29139d388e658
kateloschinina <ekaterina.loshchinina@gmail.com>
2017-08-22 11:39:00 +0100

    Fix redirection when changing the licence.

--- src/reducers/license-change.js
+++ src/reducers/license-change.js
@@ -4,6 +4,9 @@ const defaultState = {
     const pathArr = urlPath.split('/');
     // the license should be the first item (not counting the 0 index which is an empty string)
     // replace it with the new id
+    if (pathArr[0] === '') {
+      pathArr.shift();
+    }
     pathArr[1] = e.target.value;
     // navigate to the same page but with the new license id
     window.location.pathname = pathArr.join('/');
