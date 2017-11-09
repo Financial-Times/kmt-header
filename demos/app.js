c60b6182df19eb2a142883e619804ac01a9ef0ef
Simon Legg <leggsimon@gmail.com>
2017-11-09 14:29:10 +0000

    linting  🐿 v2.5.16

--- demos/app.js
+++ demos/app.js
@@ -4,60 +4,60 @@ const errorHighlight = chalk.bold.red;
 const highlight = chalk.bold.green;
 
 const app = module.exports = express({
-  name: 'public',
-  systemCode: 'kat-header-demo',
-  withFlags: false,
-  withHandlebars: true,
-  withNavigation: false,
-  withAnonMiddleware: false,
-  hasHeadCss: false,
-  viewsDirectory: '/demos',
-  partialsDirectory: process.cwd(),
-  directory: process.cwd(),
-  demo: true,
-  s3o: false
+	name: 'public',
+	systemCode: 'kat-header-demo',
+	withFlags: false,
+	withHandlebars: true,
+	withNavigation: false,
+	withAnonMiddleware: false,
+	hasHeadCss: false,
+	viewsDirectory: '/demos',
+	partialsDirectory: process.cwd(),
+	directory: process.cwd(),
+	demo: true,
+	s3o: false
 });
 
 const data = {
-  licenceList: [
-    {
-      name: 'Licence Number 1',
-      contractId: 'contract1',
-      licenceId: 'licence1'
-    },
-    {
-      name: 'Licence Number 2',
-      contractId: 'contract2',
-      licenceId: 'licence2'
-    },
-  ]
+	licenceList: [
+		{
+			name: 'Licence Number 1',
+			contractId: 'contract1',
+			licenceId: 'licence1'
+		},
+		{
+			name: 'Licence Number 2',
+			contractId: 'contract2',
+			licenceId: 'licence2'
+		},
+	]
 };
 
 app.get('/', (req, res) => {
-  res.render('demo', Object.assign({
-    title: 'Test App'
-  }, data));
+	res.render('demo', Object.assign({
+		title: 'Test App'
+	}, data));
 });
 
-function runPa11yTests() {
-  const spawn = require('child_process').spawn;
-  const pa11y = spawn('pa11y-ci');
+function runPa11yTests () {
+	const spawn = require('child_process').spawn;
+	const pa11y = spawn('pa11y-ci');
 
-  pa11y.stdout.on('data', (data) => {
-    console.log(highlight(`${data}`)); //eslint-disable-line
-  });
+	pa11y.stdout.on('data', (data) => {
+		console.log(highlight(`${data}`)); //eslint-disable-line
+	});
 
-  pa11y.stderr.on('data', (error) => {
-    console.log(errorHighlight(`${error}`)); //eslint-disable-line
-  });
+	pa11y.stderr.on('data', (error) => {
+		console.log(errorHighlight(`${error}`)); //eslint-disable-line
+	});
 
-  pa11y.on('close', (code) => {
-    process.exit(code);
-  });
+	pa11y.on('close', (code) => {
+		process.exit(code);
+	});
 }
 
 const listen = app.listen(5005);
 
 if (process.env.PA11Y === 'true') {
-  listen.then(runPa11yTests);
+	listen.then(runPa11yTests);
 }
--- gulpfile.js
+++ gulpfile.js
@@ -9,77 +9,77 @@ const livereload = require('gulp-livereload');
 let appServer;
 
 gulp.task('build', ['global-config'], function () {
-  return obt.build(gulp, {
-    js: './src/index.js',
-    sass: './main.scss',
-    buildJs: 'bundle.js',
-    buildCss: 'main.css',
-    buildFolder: 'public',
-    env: process.env.NODE_ENV
-  });
+	return obt.build(gulp, {
+		js: './src/index.js',
+		sass: './main.scss',
+		buildJs: 'bundle.js',
+		buildCss: 'main.css',
+		buildFolder: 'public',
+		env: process.env.NODE_ENV
+	});
 });
 
 gulp.task('build-page', function () {
-  return obt.build(gulp, {
-    sass: './main.scss',
-    buildCss: 'main.css',
-    buildFolder: 'public',
-    env: process.env.NODE_ENV
-  });
+	return obt.build(gulp, {
+		sass: './main.scss',
+		buildCss: 'main.css',
+		buildFolder: 'public',
+		env: process.env.NODE_ENV
+	});
 });
 
 gulp.task('install', function () {
-  return obt.install();
+	return obt.install();
 });
 
 
 gulp.task('test', function () {
-  return obt.test.npmTest(gulp);
+	return obt.test.npmTest(gulp);
 });
 
 gulp.task('serve', ['dev-add-livereload', 'build', 'build-page'], function () {
-  appServer = nodemon({
-    'script': 'demos/app.js',
-    'verbose': false,
-    'watch': false,
-    'ignore': ['*.*']
-  }).on('restart', function () {
-    /* eslint no-console: ['error', { allow: ['warn', 'log'] }] */
-    console.log('>>>>>>> nodemon app is restarting <<<<<<<<');
-  });
+	appServer = nodemon({
+		'script': 'demos/app.js',
+		'verbose': false,
+		'watch': false,
+		'ignore': ['*.*']
+	}).on('restart', function () {
+		/* eslint no-console: ['error', { allow: ['warn', 'log'] }] */
+		console.log('>>>>>>> nodemon app is restarting <<<<<<<<');
+	});
 });
 
 gulp.task('restart-server', function () {
-  appServer.restart();
+	appServer.restart();
 });
 
 gulp.task('refresh-page', ['build', 'build-page'], function () {
-  livereload.changed('src/index.js');
+	livereload.changed('src/index.js');
 });
 
 gulp.task('global-config', function () {
-  exec('node generate-config.js');
+	exec('node generate-config.js');
 });
 
 gulp.task('watch', ['serve'], function () {
-  livereload.listen({port: process.env.LIVERELOAD_PORT});
-  gulp.watch(['./style/**/*', './src/**/*'], ['refresh-page']);
-  gulp.watch('./*.*', ['restart-server']);
+	livereload.listen({port: process.env.LIVERELOAD_PORT});
+	gulp.watch(['./style/**/*', './src/**/*'], ['refresh-page']);
+	gulp.watch('./*.*', ['restart-server']);
 });
 
 gulp.task('img', function () {
-  return gulp.src('./style/images/*')
-    .pipe(imagemin({
-      progressive: true,
-      interlaced: true,
-      svgoPlugins: []
-    }))
-    .pipe(size({ showFiles: true, title: 'images compressed:' }))
-    .pipe(gulp.dest('./public/images'));
+	return gulp.src('./style/images/*')
+		.pipe(imagemin({
+			progressive: true,
+			interlaced: true,
+			svgoPlugins: []
+		}))
+		.pipe(size({ showFiles: true, title: 'images compressed:' }))
+		.pipe(gulp.dest('./public/images'));
 });
 
 gulp.task('dev-add-livereload', function () {
-  process.env.DEV_ADD_LIVERELOAD = true;
+	process.env.DEV_ADD_LIVERELOAD = true;
 });
 
 gulp.task('default', ['build', 'img']);
