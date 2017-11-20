55b029327325f80c91da363934a9a02c117a4b31
Simon Legg <leggsimon@gmail.com>
2017-11-20 18:40:23 +0000

    MOAR linting  🐿 v2.5.16

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ let appServer;
 const verifyFn = function () {
 	return obt.verify(gulp, {
 		scssLintPath: './.scss-lint.yml',
-		esLintPath: './.eslintrc'
+		esLintPath: './.eslintrc.js'
 	});
 };
 
