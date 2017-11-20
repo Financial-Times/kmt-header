bcb8533b7b70ab71d0ca2a7ca90582dce4ce2562
Simon Legg <leggsimon@gmail.com>
2017-11-20 18:15:21 +0000

    nextify  🐿 v2.5.16

---
+++ Makefile
@@ -0,0 +1,8 @@
+node_modules/@financial-times/n-gage/index.mk:
+	npm install --no-save --no-package-lock @financial-times/n-gage
+	touch $@
+
+-include node_modules/@financial-times/n-gage/index.mk
+
+test:
+	npm test
---
+++ secret-squirrel.js
@@ -0,0 +1,14 @@
+module.exports = {
+	files: {
+		allow: [
+			'.eslintrc',
+			'index.hbs',
+			'src/components/feedback-form/__snapshots__/test.js.snap'
+		],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: []
+	}
+};
