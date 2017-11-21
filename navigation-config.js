3eb31b656561954beafa9057f5b2dab5a91ee9fb
Simon Legg <leggsimon@gmail.com>
2017-11-21 15:47:23 +0000

    linting  🐿 v2.5.16

--- navigation-config.js
+++ navigation-config.js
@@ -1,34 +1,34 @@
 module.exports = {
-    nav: {
-        heading: 'Knowledge & administration tools',
-        items: [
-            {
-                name: 'Overview',
-                href: '#',
-                selected: true,
-            },
-            {
-                name: 'Content Distribution',
-                href: '#',
-            },
-            {
-                name: 'Usage Reports',
-                href: '#',
-            },
-            {
-                name: 'Licence Administration',
-                href: '#'
-            },
-            {
-                name: 'Groups',
-                href: '#',
-                flag: 'katGroupManagement'
-            },
-            {
-                name: 'Users',
-                href: '#',
-                flag: 'katUsersManagement'
-            }
-        ]
-    }
+	nav: {
+		heading: 'Knowledge & administration tools',
+		items: [
+			{
+				name: 'Overview',
+				href: '#',
+				selected: true,
+			},
+			{
+				name: 'Content Distribution',
+				href: '#',
+			},
+			{
+				name: 'Usage Reports',
+				href: '#',
+			},
+			{
+				name: 'Licence Administration',
+				href: '#'
+			},
+			{
+				name: 'Groups',
+				href: '#',
+				flag: 'katGroupManagement'
+			},
+			{
+				name: 'Users',
+				href: '#',
+				flag: 'katUsersManagement'
+			}
+		]
+	}
 };
