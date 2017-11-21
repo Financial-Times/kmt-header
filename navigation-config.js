9c577ab6998695b9540a65b6ff1d4e1e28ae6026
Simon Legg <leggsimon@gmail.com>
2017-11-21 15:31:26 +0000

    move nav config into config file  🐿 v2.5.16

---
+++ navigation-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    nav: {
+        heading: {
+            name: 'Knowledge & administration tools'
+        },
+        items: [
+            {
+                name: 'Overview',
+                href: '#',
+                selected: true,
+            },
+            {
+                name: 'Content Distribution',
+                href: '#',
+            },
+            {
+                name: 'Usage Reports',
+                href: '#',
+            },
+            {
+                name: 'Licence Administration',
+                href: '#'
+            },
+            {
+                name: 'Groups',
+                href: '#',
+                flag: 'katGroupManagement'
+            },
+            {
+                name: 'Users',
+                href: '#',
+                flag: 'katUsersManagement'
+            }
+        ]
+    }
+};
